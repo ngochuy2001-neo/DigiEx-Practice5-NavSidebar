3393f410ffdae9d641b55691e0c997201de65b6d
ngochuy2001-neo <ngochuy11010@gmail.com>
2023-11-05 15:58:03 +0700

    update icon and fix collapse glitch

--- src/components/NavSidebar.tsx
+++ src/components/NavSidebar.tsx
@@ -1,5 +1,8 @@
-import { AiOutlineMenuFold, AiOutlineMenuUnfold } from "react-icons/ai";
-import { BsFillPersonFill } from "react-icons/bs";
+import { AiOutlineMenuFold, AiOutlineMenuUnfold, AiFillHome } from "react-icons/ai";
+import { BsFillPersonFill, BsFillCreditCard2FrontFill } from "react-icons/bs";
+import { TbTransfer } from "react-icons/tb"
+import { CiCoinInsert} from "react-icons/ci"
+import { MdOutlineCurrencyExchange } from "react-icons/md"
 import { useState } from "react";
 import sidebarData from "@/utils/sidebardata";
 import Link from "next/link";
@@ -7,6 +10,24 @@ import { usePathname } from "next/navigation";
 
 const NavSideBar = () => {
   const path = usePathname();
+
+  const checkIcon = (iconName: string) => {
+    switch(iconName){
+      case 'AiFillHome':
+        return <AiFillHome />;
+      case 'BsFillCreditCard2FrontFill':
+        return <BsFillCreditCard2FrontFill />;
+      case 'CiCoinInsert':
+        return <CiCoinInsert />
+      case 'MdOutlineCurrencyExchange':
+        return <MdOutlineCurrencyExchange />;
+      case 'TbTransfer':
+        return <TbTransfer />;
+      default:
+        return null;
+    }
+  }
+
   const [isExpanded, setIsExpanded] = useState<boolean>(true);
 
   const handleFolding = () => {
@@ -41,16 +62,24 @@ const NavSideBar = () => {
                   path === value.link ? { backgroundColor: "#f4b72e" } : {}
                 }
                 className="text-white flex flex-col my-3 w-full h-fit hover:bg-blue-400 rounded-sm"
+                key={index}
               >
                 <Link
-                  className="w-full mx-3 h-[50px] flex items-center"
+                  className="w-full h-[50px] flex items-center"
                   href={value.link}
+                  style={
+                    isExpanded? {marginLeft: "5px"}: {justifyContent: "center"}
+                  }
                 >
-                  {value.text}
+                  <div className="flex justify-center" style={isExpanded? {marginRight: "5px"}: {}}>
+                  {checkIcon(value.icon)}
+                  </div>
+                  {isExpanded? value.text: ""}
                 </Link>
                 {value.submenu
                   ? value.submenu.map((subValue, index) => (
                       <Link
+                        key={index}
                         style={
                           path.indexOf(value.link) != -1
                             ? {
@@ -59,13 +88,17 @@ const NavSideBar = () => {
                                   path.indexOf(subValue.link) != -1
                                     ? "#f4b72e"
                                     : "",
+                                justifyContent: isExpanded? "start": "center",
                               }
                             : { display: "none" }
                         }
                         className="h-[50px] hover:bg-blue-300 flex items-center px-3 transition-width duration-200 bg-blue-950"
                         href={value.link + subValue.link}
                       >
-                        {subValue.text}
+                        <div className="flex items-center">
+                          {checkIcon(subValue.icon)}
+                        </div>
+                        {isExpanded? subValue.text: ""}
                       </Link>
                     ))
                   : ""}
@@ -73,11 +106,15 @@ const NavSideBar = () => {
             ))}
           </ul>
         </div>
-        <div className="w-full h-[70px] flex items-center text-white">
+        <div className="w-full h-[70px] flex items-center text-white"
+          style={isExpanded? {}:{justifyContent: "center"}}
+        >
           <div className="m-3">
             <BsFillPersonFill />
           </div>
+          {isExpanded? 
           <div>Account</div>
+          : ""}
         </div>
       </div>
     </div>
--- src/utils/sidebardata.ts
+++ src/utils/sidebardata.ts
@@ -1,4 +1,3 @@
-
 const sidebarData = [
   {
     text: "Dashboard",
